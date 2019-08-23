e9da1b0c53b5eedd17c23fe79c942c58b25ce4bf
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-23 01:24:40 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, clickbait: true
+  validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Finction"]}
 
   def validate
-    if !self.title.include?("Won't Believe")
-      false
+    if self.title != "Won't Believe" || self.title != "Secret" || self.title != "Top" || self.title != "Guess"
+      record.errors
     end
   end
 
