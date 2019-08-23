9dfc64ca929c77fbbb2da2e78b895a68e1aacd18
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-23 01:57:05 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, :if => :clickbait
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Finction"]}
-  validate :clickbait
+  #validate :clickbait
 
   def clickbait
     if !title.nil? && !title.include?("You Won't" || "Secret" || "Top" || "Guess")
