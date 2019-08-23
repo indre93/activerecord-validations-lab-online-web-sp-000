5df1cd9a7a16b39dced852744b2b3b7e4faf8ea0
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-23 01:21:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, clickbait: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Finction"]}
